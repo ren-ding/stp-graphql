f96c7f597d9a5c1e16d4bf846f02c7e0222c490c
Terry Ding <terry.adelaide@gmail.com>
2019-09-05 11:24:19 +1000

    init graphql server

---
+++ server/src/resolvers.js
@@ -0,0 +1,104 @@
+const { paginateResults } = require('./utils');
+
+module.exports = {
+    Query: {
+        launches: async (parent, args, ctx, info) => {
+            const { pageSize = 20, after} = args;
+            const { dataSources } = ctx;
+            const allLaunches = await dataSources.launchAPI.getAllLaunches();
+            allLaunches.reverse();
+            const launches = paginateResults({
+                after,
+                pageSize,
+                results:allLaunches
+            });
+            return {
+                cursor: launches.length ? launches[launches.length - 1].cursor : null,
+                hasMore: launches.length 
+                    ? launches[launches.length - 1].cursor !==
+                      allLaunches[allLaunches.length - 1].cursor
+                    : false,
+                launches
+            }
+        },
+        launch: (parent, args, ctx, info) => {
+            const { id } = args;
+            const { dataSources } = ctx;
+            return dataSources.launchAPI.getLaunchById({launchId: id})
+        },
+        me: async (parent, args, ctx, info) => {
+            const { dataSources } = ctx;
+            dataSources.userAPI.findOrCreateUser()
+        }
+    },
+    Mutation: {
+        login: async (parent, args, ctx, info) => {
+            const { email } = args;
+            const { dataSources } = ctx;
+            const user = await dataSources.userAPI.findOrCreateUser({email});
+            if(user) return Buffer.from(email).toString('base64');
+        },
+        bookTrips: async (parent, args, ctx, info) => {
+            const { launchIds } = args;
+            const { dataSources } = ctx;
+            const results = await dataSources.userAPI.bookTrips({ launchIds });
+            const launches = await dataSources.launchAPI.getLaunchesByIds({
+                launchIds,
+            });
+        
+            return {
+                success: results && results.length === launchIds.length,
+                message:
+                results.length === launchIds.length
+                    ? 'trips booked successfully'
+                    : `the following launches couldn't be booked: ${launchIds.filter(
+                        id => !results.includes(id),
+                    )}`,
+                launches,
+            };
+        },
+        cancelTrip: async (parent, args, ctx, info) => {
+            const { launchIds } = args;
+            const { dataSources } = ctx;
+            const result = await dataSources.userAPI.cancelTrip({ launchId });
+        
+            if (!result)
+                return {
+                success: false,
+                message: 'failed to cancel trip',
+                };
+        
+            const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+            return {
+                success: true,
+                message: 'trip cancelled',
+                launches: [launch],
+            };
+        },
+    },
+    Mission: {
+        missionPatch:(parent, args, ctx, info) => {
+            const {mission} = parent;
+            const {size = 'LARGE'} = args
+            return size === 'SMALL'
+                ? mission.missionPatchSmall
+                : mission.missionPatchLarge;
+        },
+    },
+    Launch: {
+        isBooked: async (parent, args, ctx, info) => {
+            const { dataSources } = ctx;
+            return dataSources.userAPI.isBookedOnLaunch({ launchId: parent.id });
+        }
+    },
+    User: {
+        trips: async (parent, args, ctx, info) => {
+            const { dataSources } = ctx;
+            const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+            
+            if(!launchIds.length) return [];
+
+            return (dataSources.launchAPI.getLaunchesByIds({ launchIds }) || [] );
+        }
+    }
+};
---
+++ server/src/schema.js
@@ -0,0 +1,61 @@
+const { gql } = require('apollo-server');
+const typeDefs = gql`
+    type Query {
+        launches(
+            pageSize: Int
+            after: String
+        ): LaunchConnection!
+        launch(id: ID!): Launch
+        me: User
+    }
+
+    type LaunchConnection {
+        cursor: String!
+        hasMore: Boolean!
+        launches: [Launch]!
+    }
+
+    type Launch {
+        id:ID!
+        site:String
+        mission:Mission
+        rocket:Rocket
+        isBooked:Boolean!
+    }
+
+    type Rocket {
+        id:ID!
+        name:String
+        type:String
+    }
+
+    type User {
+        id: ID!
+        email:String!
+        trips:[Launch]!
+    }
+
+    type Mission {
+        name:String
+        missionPatch(mission: String, size: PatchSize): String
+    }
+
+    type Mutation {
+        bookTrips(launchIds: [ID]!): TripUpdateResponse!
+        cancelTrip(launchId: ID!): TripUpdateResponse!
+        login(email: String): String
+    }
+
+    type TripUpdateResponse {
+        success: Boolean!
+        message: String
+        launches:[Launch]
+    }
+
+    enum PatchSize {
+        SMALL
+        LARGE
+    }
+`;
+
+module.exports = typeDefs;
